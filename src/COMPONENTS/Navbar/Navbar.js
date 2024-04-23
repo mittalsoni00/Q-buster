6487c071988647fcd033642d41e1747fcd50d21a
mittalsoni00 <142537504+mittalsoni00@users.noreply.github.com>
2024-04-23 10:59:35 +0530

    Update Navbar.js

--- src/COMPONENTS/Navbar/Navbar.js
+++ src/COMPONENTS/Navbar/Navbar.js
@@ -33,14 +33,16 @@ const Navbar = ({ reloadnavbar }) => {
                 <img src={logo} alt='logo' className='logo' />
 
                 <div className='searchbar'>
-                    <input typ="text" placeholder="Search for products and categries" className='search' />
+                    // <input typ="text" placeholder="Search for products and categries" className='search' />
 
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                        </svg>
+                    // <button>
+                    //     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    //         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    //     </svg>
 
-                    </button>
+                    // </button>
+            <button className='mainbutton1'
+             >Order Now</button>
                 </div>
 
                 <div className='right'>
